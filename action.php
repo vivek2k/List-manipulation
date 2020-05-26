832bf5345372d1a93637f25b298a7dc2f6561ca7
Vivekanandan Mariappan <vivekanandan2e3@gmail.com>
2020-05-26 17:43:05 +0530

    Add files via upload

--- action.php
+++ action.php
@@ -3,18 +3,30 @@
         <?php
             $l1 = $_POST['list1'];
             // echo "$l1";
+            $l1List = explode("\n", $l1);
+            $il1List = implode(" ", $l1List);
             // echo "<br>";
-            $strl1 = strval($l1);
-            $size1 = substr_count($l1, ' ');
+            $strl1 = strval($il1List);
+            $size1 = substr_count($il1List, ' ');
             // echo $size1;
+
+            // echo "<br>";
+            // echo "<br>";
+            // echo "Imploaded list : ";
+            // echo $imploadedl1List;
+            // echo "<br>";
+            // echo "<br>";
+            $strl1 = strval($il1List);
 
             $l2 = $_POST['list2'];
             // echo "$l2";
             // echo "<br>";
-            $strl2 = strval($l2);
+            $l2List = explode("\n", $l2);
+            $il2List = implode(" ", $l2List);
+            $strl2 = strval($il2List);
             $size2 = substr_count($strl2, ' ');
-                // echo $size2;
-                
+            // echo $size2;
+
             $result = shell_exec("python ./python.py $size1 $size2 $strl1 $strl2");
             // echo $result;
             $list = explode('.', $result);
@@ -22,9 +34,53 @@
             echo "<br>";
             echo $list[1];
             echo "<br>";
-            echo $list[2];
             echo "<br>";
-            echo $list[3];
+            // echo ($size1);
+            echo "<br>";
+            // echo $size2;
+            $strRem = strval($list[3]); 
+            echo strlen($strRem);
+            $strAdd = strval($list[2]); 
+            echo strlen($strAdd);
+            echo $strAdd;
+            // echo $listTemp;
+            // echo gettype($list);
         ?>
+
+        <form>
+        To Remove : <textarea name="addList" form="stockList" cols="30" rows="10" wrap="hard">
+        <?php 
+            for($i=0;$i<strlen($strRem);$i=$i+1) {
+                if($strRem[$i]=='\''||$strRem[$i]=='{'|$strRem[$i]=='}'||$strRem[$i]==',') {
+
+                    continue;
+                }
+                else if($strAdd[$i]== ' ') {
+                    echo "\n";
+                }
+                else {
+                    echo $strRem[$i];
+
+                }  
+            }
+
+        ?>
+        </textarea>    
+        To Add : <textarea name="removeList" form="stockList" cols="30" rows="10" wrap="hard">
+        <?php 
+            for($i=0;$i<strlen($strAdd);$i=$i+1) {
+                if($strAdd[$i]=='\''||$strAdd[$i]=='{'|$strAdd[$i]=='}'||$strAdd[$i]==',') {
+                    continue;
+                }
+                else if($strAdd[$i]== ' ') {
+                    echo "\n";
+                }
+                else {
+                    echo $strAdd[$i];
+
+                }  
+            }
+        ?>
+        </textarea>    
     </body>
 </html>
--- python.py
+++ python.py
@@ -8,15 +8,15 @@ size1 = sys.argv[1:2]
 size2 = sys.argv[2:3]
 
 l1 = sys.argv[3:3+int(size1[0])+1]
-
 print("First List : " + format(l1) + ".")
 
 l2 = sys.argv[3+int(size1[0])+1:]
 
 print("Second List : " + format(l2) + ".")
-print("\n")
+
 toAdd = set(l1) - set(l2)
 toRemove = set(l2) - set(l1)
-print("You have to add : " + format(toAdd) +".")
+                                                                    #toAdd and ToRemove are of type <set>
+print(format(toAdd) +".")
 
-print("You have to remove : " + format(toRemove) + ".")
+print(format(toRemove) + ".")
