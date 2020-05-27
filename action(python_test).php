78405aba887fc6c5012ef7ef358ecf3357dc67c8
Vivekanandan Mariappan <vivekanandan2e3@gmail.com>
2020-05-27 14:21:10 +0530

    created new action.php
    changed action.php to action(python_test).php and created new action.php

---
+++ action(python_test).php
@@ -0,0 +1,55 @@
+<html>
+    <body style="margin:100px;background-color:#F5F5F5">
+        <?php
+            $l1 = $_POST['list1'];
+            // echo "$l1";
+            $l1List = explode("\n", $l1);
+            $il1List = implode(" ", $l1List);
+            // echo "<br>";
+            $strl1 = strval($il1List);
+            $size1 = substr_count($il1List, ' ');
+            // echo $size1;
+
+            // echo "<br>";
+            // echo "<br>";
+            // echo "Imploaded list : ";
+            // echo $imploadedl1List;
+            // echo "<br>";
+            // echo "<br>";
+            $strl1 = strval($il1List);
+
+            $l2 = $_POST['list2'];
+            // echo "$l2";
+            // echo "<br>";
+            $l2List = explode("\n", $l2);
+            $il2List = implode(" ", $l2List);
+            $strl2 = strval($il2List);
+            $size2 = substr_count($strl2, ' ');
+            // echo $size2;
+
+            $result = shell_exec("python ./python.py $size1 $size2 $strl1 $strl2");
+            // echo $result;
+            $list = explode('.', $result);
+            echo $list[0];
+            echo "<br>";
+            echo $list[1];
+            echo "<br>";
+            echo "<br>";
+            // echo ($size1);
+            echo "<br>";
+            // echo $size2;
+            $strRem = strval($list[3]); 
+            // echo strlen($strRem);
+            $strAdd = strval($list[2]); 
+            // echo strlen($strAdd);
+            // echo $strAdd;
+            // echo $listTemp;
+            // echo gettype($list);
+        ?>
+
+        <form style="height:500px;width:700px">
+        <p style="color:Black;font-size:28px;">To Add :</p><textarea name="removeList" form="stockList" cols="30" rows="10" wrap="hard" style=""><?php for($i=0;$i<strlen($strAdd);$i=$i+1) {if($strAdd[$i]=='\''||$strAdd[$i]=='{'|$strAdd[$i]=='}'||$strAdd[$i]==',') {continue;}else if($strAdd[$i]== ' ') {echo "\n";}else {echo $strAdd[$i];}}?></textarea>    
+        <p style="color:Black;font-size:28px;;">To Remove : </p><textarea name="addList" form="stockList" cols="30" rows="10" wrap="hard" style=""><?php for($i=0;$i<strlen($strRem);$i=$i+1) {if($strRem[$i]=='\''||$strRem[$i]=='{'|$strRem[$i]=='}'||$strRem[$i]==',') {continue;}else if($strRem[$i]== ' ') {echo "\n";}else {echo $strRem[$i];}}?></textarea>    
+        
+    </body>
+</html>
--- action.php
+++ action.php
@@ -3,83 +3,54 @@
         <?php
             $l1 = $_POST['list1'];
             // echo "$l1";
+            // echo "<br>";
             $l1List = explode("\n", $l1);
-            $il1List = implode(" ", $l1List);
-            // echo "<br>";
-            $strl1 = strval($il1List);
-            $size1 = substr_count($il1List, ' ');
+            $size1 = sizeof($l1List);
             // echo $size1;
-
             // echo "<br>";
-            // echo "<br>";
-            // echo "Imploaded list : ";
-            // echo $imploadedl1List;
-            // echo "<br>";
-            // echo "<br>";
-            $strl1 = strval($il1List);
 
             $l2 = $_POST['list2'];
             // echo "$l2";
             // echo "<br>";
             $l2List = explode("\n", $l2);
-            $il2List = implode(" ", $l2List);
-            $strl2 = strval($il2List);
-            $size2 = substr_count($strl2, ' ');
+            $size2 = sizeof($l2List);
             // echo $size2;
+            // echo "<br>";
 
-            $result = shell_exec("python ./python.py $size1 $size2 $strl1 $strl2");
-            // echo $result;
-            $list = explode('.', $result);
-            echo $list[0];
-            echo "<br>";
-            echo $list[1];
-            echo "<br>";
-            echo "<br>";
-            // echo ($size1);
-            echo "<br>";
-            // echo $size2;
-            $strRem = strval($list[3]); 
-            // echo strlen($strRem);
-            $strAdd = strval($list[2]); 
-            // echo strlen($strAdd);
-            // echo $strAdd;
-            // echo $listTemp;
-            // echo gettype($list);
+            $l1List = array_map('trim', $l1List);            
+            // var_dump($l1List);
+            // echo "<br>";
+            $l2List = array_map('trim', $l2List);
+            // var_dump($l2List);
+
+            // echo "<br>";
+            // echo "<br>";
+            // echo "<br>";
+            
+            $toAdd = array_diff($l1List, $l2List);
+            $toRemove = array_diff($l2List, $l1List);
+            // echo gettype($toAdd);
+            // echo "<br>";    
+            // echo sizeof($toAdd);
+            // var_dump($toAdd);
+            // echo "<br>";
+            // echo gettype($toRemove);
+            // echo "<br>";
+            // echo sizeof($toRemove);
+            // var_dump($toRemove);
+
+            // echo "<br>";
+            // print_r(implode("<br>", $toAdd));
+            // echo "<br>";
+
+            // echo "<br>";
+            // print_r(implode("<br>", $toRemove));
+            // echo "<br>";
+
         ?>
 
-        <form style="height:500px;width:700px">
-        <p style="color:Black;font-size:28px;">To Add :</p><textarea name="removeList" form="stockList" cols="30" rows="10" wrap="hard" style="">
-        <?php 
-            for($i=0;$i<strlen($strAdd);$i=$i+1) {
-                if($strAdd[$i]=='\''||$strAdd[$i]=='{'|$strAdd[$i]=='}'||$strAdd[$i]==',') {
-                    continue;
-                }
-                else if($strAdd[$i]== ' ') {
-                    echo "\n";
-                }
-                else {
-                    echo $strAdd[$i];
-
-                }  
-            }
-        ?>
-        </textarea>    
-        <p style="color:Black;font-size:28px;;">To Remove : </p><textarea name="addList" form="stockList" cols="30" rows="10" wrap="hard" style="">
-        <?php 
-            for($i=0;$i<strlen($strRem);$i=$i+1) {
-                if($strRem[$i]=='\''||$strRem[$i]=='{'|$strRem[$i]=='}'||$strRem[$i]==',') {
-                    continue;
-                }
-                else if($strRem[$i]== ' ') {
-                    echo "\n";
-                }
-                else {
-                    echo $strRem[$i];
-
-                }  
-            }
-        ?>
-        </textarea>    
-        
+<form style="height:500px;width:700px">
+        <p style="color:Black;font-size:28px;">To Add :</p><textarea name="removeList" form="stockList" cols="30" rows="10" wrap="hard" style=""><?php print_r(implode("\n",$toAdd)) ?></textarea>    
+        <p style="color:Black;font-size:28px;;">To Remove : </p><textarea name="addList" form="stockList" cols="30" rows="10" wrap="hard" style=""><?php print_r(implode("\n", $toRemove)); ?></textarea>    
     </body>
 </html>
