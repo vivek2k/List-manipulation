d35f1e700c497dfbe6b0456d95092d86475dd6ef
Vivekanandan Mariappan <vivekanandan2e3@gmail.com>
2020-05-31 19:56:42 +0530

    added style.css and changed basic skeleton

--- action.php
+++ action.php
@@ -1,5 +1,10 @@
 <html>
-    <body style="margin:100px;background-color:#F5F5F5">
+    <head>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css" type="text/css">
+    </head>
+    <body style=";background-color:#F5F5F5;margin-top:15px">
+    
         <?php
             $l1 = $_POST['list1'];
             // echo "$l1";
@@ -48,9 +53,29 @@
             // echo "<br>";
 
         ?>
+    <div><p class="header"> List comparator </p></div>
+    <div class="container border" style="height: fit-content;width: fit-content;">
+        <div class="row">
+            <div class="col-6">
+                <p >List to Add:</p>
+            </div>
+            <div class="col-6">
+                <p >List to Remove:</p>
+            </div>
+        </div>
+        <form action="./action.php" method="POST" id="stockList" style="height:auto;width:auto;">
+            <div class="row">
+                <div class="col "><textarea name="removeList" form="stockList" cols="30" rows="10" wrap="hard" style=""><?php print_r(implode("\n",$toAdd)) ?></textarea></div>
+                <div class="col "><textarea name="addList" form="stockList" cols="30" rows="10" wrap="hard" style=""><?php print_r(implode("\n", $toRemove)); ?></textarea></div>  
+           </div>
+        </div>
+    </div>
+    <div style="float:right;position:absolute;bottom:10px;right:10px">
+            <span> Created by Vivekanandan. </span><br>
+            <a style="float:right" href="https://github.com/vivek2k">Github</a>
+            <br>
+            <a style="float:right" href="https://www.instagram.com/vivekanandan._.mariappan/">Instagram</a>
 
-<form style="height:500px;width:700px">
-        <p style="color:Black;font-size:28px;">To Add :</p><textarea name="removeList" form="stockList" cols="30" rows="10" wrap="hard" style=""><?php print_r(implode("\n",$toAdd)) ?></textarea>    
-        <p style="color:Black;font-size:28px;;">To Remove : </p><textarea name="addList" form="stockList" cols="30" rows="10" wrap="hard" style=""><?php print_r(implode("\n", $toRemove)); ?></textarea>    
+        </div>
     </body>
 </html>
